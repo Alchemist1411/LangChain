9250a38c5b1e1eb2e339954e62c7fbf3ee5b925a
ambesh333 <ambeshgaunker@gmail.com>
2025-02-20 19:53:43 +0530

    feat:tools

--- src/toolConfig/allTools.ts
+++ src/toolConfig/allTools.ts
@@ -1,7 +1,9 @@
-import llm from "../createInstance";
-import add from "../tools/addTool";
-import multiply from "../tools/multiplyTool";
 
-export const tools = [add, multiply];
+import { llm } from "../createInstance";
+import { estimateGas } from "../tools/estimate_gas";
+import { getWalletBalance } from "../tools/get_balance";
+import { sendSonicToken } from "../tools/send_token";
+
+export const tools = [getWalletBalance, estimateGas, sendSonicToken];
 export const toolsByName = Object.fromEntries(tools.map((tool) => [tool.name, tool]));
 export const llmWithTools = llm.bindTools(tools);
