e50b5eb15b0f7cd3f7740e6af0df975c80de8544
ambesh333 <ambeshgaunker@gmail.com>
2025-02-23 13:09:54 +0530

    feat:chart_tool

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import agentRouter from "./routes/agent";
 import healthCheckRouter from "./routes/healthCheck";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 dotenv.config();
--- src/toolConfig/allTools.ts
+++ src/toolConfig/allTools.ts
@@ -1,8 +1,9 @@
 import llm from "../createInstance";
+import getSymbol from "../tools/chart";
 import estimateGas from "../tools/estimate_gas";
 import getWalletBalance from "../tools/get_balance";
 import sendSonicToken from "../tools/send_token";
 
-export const tools = [getWalletBalance, estimateGas, sendSonicToken];
+export const tools = [getWalletBalance, estimateGas, sendSonicToken, getSymbol];
 export const toolsByName = Object.fromEntries(tools.map((tool) => [tool.name, tool]));
 export const llmWithTools = llm.bindTools(tools);
